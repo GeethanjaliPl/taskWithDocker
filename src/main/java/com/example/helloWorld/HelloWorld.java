a247c5eec820eb1dc5df2f87abbfd06a34563a96
Geetha P <lgeethaap@gmail.com>
2026-04-10 12:41:56 +0530

    adding spring boot

--- src/main/java/com/example/helloWorld/HelloWorld.java
+++ src/main/java/com/example/helloWorld/HelloWorld.java
@@ -1,10 +1,11 @@
 package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
 public class HelloWorld {
 
